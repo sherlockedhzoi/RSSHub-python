2905329b084991c7ddef959edaea59deda088dad
Sherlocked_hzoi <78630956+sherlockedhzoi@users.noreply.github.com>
2025-02-06 08:48:22 +0000

    add ccsp feed

---
+++ rsshub/spiders/ccf/ccsp.py
@@ -0,0 +1,29 @@
+import json
+import requests
+from bs4 import BeautifulSoup
+
+domain = 'https://www.ccf.org.cn'
+
+def parse(post):
+    views = post.find('div', class_='Meta').find('span', class_='ViewCount').span.text
+    comments = post.find('div', class_='Meta').find('span', class_='CommentCount').span.text
+    return {
+        'title': post.find('div', class_='info').find('a', class_='name').text,
+        'description': post.find('div', class_='info').find('div', class_='summary').a.text,
+        'link': f'{domain}{post.find('div', class_='info').find('a', class_='name')['href']}',
+        'pubDate': post.find('div', class_='info').find('div', class_='time').text,
+        'author': 'CCF组委会'
+    }
+
+def ctx():
+    url = f'{domain}/ccsp/tzgg'
+    html = requests.get(url).text
+    soup = BeautifulSoup(html, 'html.parser')
+    posts = soup.find('div', class_='listbox').find_all('div', class_='item')     
+    return {
+        'title': 'CCF - CCSP',
+        'link': url,
+        'description': 'CCSP竞赛(Collegiate Computer Systems & Programming Contest，大学生计算机系统与程序设计竞赛)，由中国计算机学会(CCF)于2016年发起的一个面向大学生的竞赛，每年举办一次，考察的是算法、编程以及计算机系统设计能力，旨在进一步提高计算机教育质量，使学生通过竞赛进一步学习和掌握计算机系统知识，同时对高校计算机教育产生引领作用。',
+        'author': 'Sherlocked HZOI',
+        'items': list(map(parse, posts))
+    }
