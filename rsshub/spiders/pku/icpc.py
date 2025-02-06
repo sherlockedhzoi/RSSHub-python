86232dd5f39b99c919c059fc6db182e73ee98fb9
Sherlocked_hzoi <78630956+sherlockedhzoi@users.noreply.github.com>
2025-02-06 13:30:38 +0000

    try fix error

--- rsshub/spiders/pku/icpc.py
+++ rsshub/spiders/pku/icpc.py
@@ -5,8 +5,8 @@ domain = 'http://icpc.pku.edu.cn'
 
 def parse(post):
     return {
-        'title': post.a.text.split('[')[0].encode('latin1'),
-        'description': post.a.text.encode('latin1'),
+        'title': post.a.text.split('[')[0].encode('utf-8').decode('utf-8'),
+        'description': post.a.text.encode('utf-8').decode('utf-8'),
         'link': post.a['href'],
         'pubDate': post.a.span.text.strip('[]'),
         'author': 'ICPC 北京总部'
