6cb4b8ed6b04403eb570f010551fbcd1a54bce2a
Sherlocked_hzoi <78630956+sherlockedhzoi@users.noreply.github.com>
2025-02-06 08:16:26 +0000

    fix gplt description

--- rsshub/spiders/pintia/gplt.py
+++ rsshub/spiders/pintia/gplt.py
@@ -18,9 +18,9 @@ def ctx():
     url = 'https://gplt.pintia.cn/api/cached/notifications'
     posts = requests.get(url).json()['data']['notifications']
     return {
-        'title': '蓝桥云课 - 蓝桥杯 - 大赛通知',
+        'title': '中国高校计算机大赛 - 团体程序设计天梯赛 - 赛事通知',
         'link': f'{domain}/notification',
-        'description': '蓝桥杯全国软件和信息技术专业人才大赛由工业和信息化部人才交流中心主办，包括北大、清华等在内的全国31个省市自治区1200多所院校参加，每年参赛人数超过30000人。',
+        'description': '团体程序设计天梯赛是中国高校计算机大赛的竞赛版块之一，赛旨在提升学生计算机问题求解水平，增强学生程序设计能力，培养团队合作精神，提高大学生的综合素质，同时丰富校园学术气氛，促进校际交流，提高全国高校的程序设计教学水平。',
         'author': 'Sherlocked HZOI',
         'items': list(map(parse, posts))
     }
