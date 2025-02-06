5e3030f93c51d504f5fbb6431f552c92ef6f4e34
Sherlocked_hzoi <78630956+sherlockedhzoi@users.noreply.github.com>
2025-02-06 11:15:45 +0000

    add cet feed

---
+++ rsshub/spiders/neea/cet.py
@@ -0,0 +1,27 @@
+import json
+import requests
+from bs4 import BeautifulSoup
+
+domain = 'https://cet.neea.edu.cn'
+
+def parse(post):
+    return {
+        'title': post.find('span', id='ReportIDname').a.text,
+        'description': post.find('span', id='ReportIDname').a.text + post.find('span', id='ReportIDIssueTime').text,
+        'link': domain + post.find('span', id='ReportIDname').a['href'],
+        'pubDate': post.find('span', id='ReportIDIssueTime').text,
+        'author': '中国教育考试网'
+    }
+
+def ctx():
+    url = f'{domain}/html1/category/16093/1124-1.htm'
+    html = requests.get(url).text
+    soup = BeautifulSoup(html, 'html.parser')
+    posts = map(lambda span: span.parent, soup.find_all('span', id='ReportIDname'))
+    return {
+        'title': 'NEEA - NCRE',
+        'link': url,
+        'description': '''全国计算机等级考试（National Computer Rank Examination，简称NCRE），是经原国家教育委员会（现教育部）批准，由教育部教育考试院（原教育部考试中心）主办，面向社会，用于考查应试人员计算机应用知识与技能的全国性计算机水平考试体系。NCRE开考之后，受到社会广泛关注和认可，为我国信息化技术人才的培养做出了重要贡献。''',
+        'author': 'Sherlocked HZOI',
+        'items': list(map(parse, posts))
+    }
